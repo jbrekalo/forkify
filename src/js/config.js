75d1d19bab478dc07ecc33573bdb070160d71cd4
jbrekalo <josip.brekalo@hotmail.com>
2023-08-30 15:14:05 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'aef26f1e-68a1-4728-a8e1-f7e5456beefd';
+export const MODAL_CLOSE_SEC = 2.5;
